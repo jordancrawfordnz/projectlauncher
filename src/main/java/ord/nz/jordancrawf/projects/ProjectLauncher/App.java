a1f510d78686786fc17eb13ce05a0bdf2ea481ff
Jordan Crawford <jordan.crawford@me.com>
2015-03-07 15:28:16 +1300

    Use App.java as the main interface with the user.

--- src/main/java/ord/nz/jordancrawf/projects/ProjectLauncher/App.java
+++ src/main/java/ord/nz/jordancrawf/projects/ProjectLauncher/App.java
@@ -6,8 +6,43 @@ package ord.nz.jordancrawf.projects.ProjectLauncher;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public static void main(String args[])
+	{
+		if(args.length == 0)
+		{
+			for(Project currentProject : ProjectLauncher.getInstance().getProjects())
+			{
+				System.out.println(" - " + currentProject);
+			}
+			return;
+		}
+		if(args.length == 1)
+		{
+			for(Project currentProject : ProjectLauncher.getInstance().getProjects())
+			{
+			
+				if(currentProject.getName().equals(args[0]))
+				{
+					if(currentProject.Launch())
+					{
+						System.out.println("Cool. Enjoy!");
+						System.exit(0);
+					}
+					else
+					{
+						System.err.println("Sorry. Something bad happened, am I missing required classes? You may want to check your project configuration.");
+						System.exit(1);
+					}
+				}
+				else
+				{
+					System.err.println("Darn! \"" + args[0] + "\" isn't a project I know about. Use 'project' to list all projects I know :)");
+				}
+			}
+			// check project, setup project
+			return;
+		}
+		System.err.print("Invalid argument provided! Use: 'project' to list. or 'project [project_name]' to launch a project. Have a great day.");
+	}	
 }
